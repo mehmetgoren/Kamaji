2794a4facd40e743b3f88074e95a331ab6de9f76
Mehmet Goren <mehmetgoren@outlook.com>
2018-08-29 00:34:09 +0300

    1

---
+++ Kamaji.Worker/WorkerBase.cs
@@ -0,0 +1,26 @@
+﻿namespace Kamaji.Worker
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public abstract class WorkerBase : IWorker
+    {
+        protected abstract Task<object> Run_Internal(IObserver observer, string asset, IScanRepository repository, object args);
+        public async Task<WorkerResult> Run(IObserver observer, string asset, IScanRepository repository, object args)
+        {
+            try
+            {
+                object result = await Run_Internal(observer, asset, repository, args);
+                return new WorkerResult(result);
+            }
+            catch (Exception ex)
+            {
+                return new WorkerResult(null, false, ex.Message);
+            }
+        }
+
+
+        public abstract Task SetupEnvironment();
+        public abstract void Dispose();
+    }
+}
